b8e2d376a5a81cc728dcad4f58c816cdb3153e22
PeterChou2 <peterisawesome05@gmail.com>
2021-03-06 20:08:02 -0500

    dev

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ var app = express();
 if(process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
     if (req.header('x-forwarded-proto') !== 'https')
-      res.redirect(`https://${req.header('host')}${req.url}`)
+      res.redirect(`https://${req.header('host')}${req.url}`);
     else
       next()
   });
