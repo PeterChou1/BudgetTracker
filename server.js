6fc7ff9f384b107f6084ef0f4ef23f59cf784ada
PeterChou1 <peter.chou@mail.utoronto.ca>
2024-10-15 19:22:23 -0400

    print client id

--- server.js
+++ server.js
@@ -39,6 +39,11 @@ const plaid = require("plaid");
 const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
 const PLAID_SECRET = process.env.PLAID_SECRET;
 const PLAID_ENV = process.env.PLAID_ENV || "sandbox";
+
+console.log(PLAID_CLIENT_ID);
+console.log(PLAID_SECRET);
+console.log(PLAID_ENV);
+
 const client = new plaid.Client({
   clientID: PLAID_CLIENT_ID,
   secret: PLAID_SECRET,
